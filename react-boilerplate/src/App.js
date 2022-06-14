69c91784482e331a5370099d0dad68cc2a1324a3
Frederick Tan <ftan2@scu.edu>
2022-06-13 23:47:08 -0700

    added react-boilerplate assignment

---
+++ react-boilerplate/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Test from "./components/test";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Test />
+    </div>
+  );
+}
---
+++ react-boilerplate/src/components/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./test.css";
+
+export default function Test() {
+  return (
+    <div>
+      <h2>Testing CSS loader</h2>
+    </div>
+  );
+}
